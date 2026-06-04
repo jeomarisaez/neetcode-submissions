377f72f2a2254709be4c48b8b539785bb84ef945
Jeo Mari Saez <jeomarisaez7@gmail.com>
2026-06-04 18:16:22 +0800

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_map = {}
+        t_map = {}
+        for c in s:
+            if c not in s_map.keys():
+                s_map[c] = 1
+            else:
+                s_map[c] += 1
+        for d in t:
+            if d not in t_map.keys():
+                t_map[d] = 1
+            else:
+                t_map[d] += 1
+        if s_map == t_map:
+            return True
+        else:
+            return False
+
+            
